c6e0b4f5931287074f4c43e33e6b0971cc30deb3
bertrandmalet <malet.bertrand@gmail.com>
2012-06-20 23:33:23 +0200

    Adding multiple metrics to matrix graph

--- xhprof_html/matrix.php
+++ xhprof_html/matrix.php
@@ -33,19 +33,39 @@ $totals;
 
 $xhprof_runs_impl = new XHProfRuns_Default();
 $raw_data = $xhprof_runs_impl->get_run($run, $totals, $desc_unused);
-$flat_data = xhprof_compute_flat_info($raw_data[0], $overall_totals);
-unset($raw_data[0]['main()']);
+
+$raw_data_threshold = array();
+foreach ($raw_data[0] as $parent_child=>$info) {
+    if ($info['wt'] > $threshold) {
+        $raw_data_threshold[$parent_child] = $info;
+    }
+}
+$flat_data = xhprof_compute_flat_info($raw_data_threshold, $overall_totals);
+unset($raw_data_threshold['main()']);
 
 $symbols = array_flip(array_keys($flat_data));
 $links = array();
+$maxMetrics = array('wt' => 0, 'ct' => 0, 'mu' => 0, 'pmu' => 0);
 
-foreach ($raw_data[0] as $parent_child=>$info) {
+foreach ($raw_data_threshold as $parent_child=>$info) {
     list($parent, $child) = xhprof_parse_parent_child($parent_child);
     $links[] = array(
         'source' => $symbols[$parent],
         'target' => $symbols[$child],
         'info' => $info
     );
+    if ($maxMetrics['wt'] < $info['wt']) {
+        $maxMetrics['wt'] = $info['wt'];
+    }
+    if ($maxMetrics['ct'] < $info['ct']) {
+        $maxMetrics['ct'] = $info['ct'];
+    }
+    if ($maxMetrics['mu'] < $info['mu']) {
+        $maxMetrics['mu'] = $info['mu'];
+    }
+    if ($maxMetrics['pmu'] < $info['pmu']) {
+        $maxMetrics['pmu'] = $info['pmu'];
+    }
 }
 
 $symbols = array_flip($symbols);
@@ -59,20 +79,42 @@ svg {
     font: 8px sans-serif;
 }
 .background {
-    fill: #EEEEEE;
+    fill: #FFFFFF;
 }
 line {
-    stroke: #FFFFFF;
+    stroke: #EEE;
 }
 text.active {
     fill: #0066FF;
-    font: 15px sans-serif;
     font-weight: bold;
 }
-.cell {
+.cell_wt {
     fill: #0066FF;
 }
+.cell_ct {
+    fill: red;
+}
+.cell_mu {
+    fill: green;
+}
+.cell_pmu {
+    fill: yellow;
+}
+.cell_sep {
+    fill: #EEE;
+}
 </style>
+
+Show : 
+<select id="show">
+    <option value="wt" selected="selected">walltime</option>
+    <option value="ct">count</option>
+    <option value="cpu">cpu</option>
+    <option value="mu">memory usage</option>
+    <option value="pmu">peak memory usage</option>
+</select>
+
+Sort by : 
 <select id="order">
 <?php foreach (array_keys($symbols[0]) as $k):?>
 <option value="<?php echo $k?>"><?php echo $k?></option>
@@ -82,6 +124,8 @@ text.active {
 <script type="text/javascript">
 var symbols = <?php echo json_encode($symbols);?>;
 var links = <?php echo json_encode($links);?>;
+var maxMetrics = <?php echo json_encode($maxMetrics);?>;
+var nodeKeys = d3.keys(links[0].info);
 var margin = {top: 120, right: 0, bottom: 10, left: 120},
     width = 1200, 
     height = width;
@@ -98,13 +142,16 @@ var svg = d3.select("body").append("svg")
 var orders = {
     name: d3.range(nbSymbols).sort(function(a, b) { return d3.ascending(symbols[a].name, symbols[b].name); }),
 };
+var z = {};
 d3.keys(symbols[0]).forEach(function(key, i) {
-	orders[key] = d3.range(nbSymbols).sort(function(a, b) { return symbols[b][key] - symbols[a][key]; });
+	if (key != 'name') {
+	    orders[key] = d3.range(nbSymbols).sort(function(a, b) { return symbols[b][key] - symbols[a][key]; });
+	    z[key] = d3.scale.linear().domain([0, maxMetrics[key]]).clamp(true);
+	}
 });
 
-var z = d3.scale.linear().domain([0, 50]).clamp(true);
 var x = d3.scale.ordinal().rangeBands([0, width]);
-x.domain(orders.wt);
+x.domain(orders.name);
 
 svg.append("rect")
     .attr("class", "background")
@@ -114,12 +161,16 @@ svg.append("rect")
 
 
 symbols.forEach(function(symbol, i) {
-    matrix[i] = d3.range(nbSymbols).map(function(j) { return {x: j, y: i, z: 0}; });
+    matrix[i] = d3.range(nbSymbols).map(function(j) {
+        var info = {};
+    	nodeKeys.forEach(function (k) {info[k]=0;});
+        return {x: j, y: i, info: info}; 
+    });
 });
 
 links.forEach(function(link) {
-    matrix[link.source][link.target].z = link.info.wt;
-    matrix[link.target][link.source].z = link.info.wt;
+    matrix[link.source][link.target].info = link.info;
+    matrix[link.target][link.source].info = link.info;
 });
 
 var row = svg.selectAll(".row")
@@ -157,48 +208,68 @@ column.append("text")
     .text(function(d, i) { return symbols[i].name; });
 
 function row(row) {
-    var cell = d3.select(this).selectAll(".cell")
-        .data(row.filter(function(d) { return d.z; }))
-        .enter().append("rect")
-        .attr("class", "cell")
-        .attr("x", function(d, i) { return x(d.x); })
-        .attr("width", x.rangeBand())
-        .attr("height", x.rangeBand())
-        .style("fill-opacity", function(d) { return z(d.z); })
-        .on("mouseover", mouseover)
-        .on("mouseout", mouseout)
-        .on("click", function (p) {
-            alert(symbols[p.y].name+'/'+symbols[p.x].name+'='+p.z);
-        });
+	var g = this;
+	
+	d3.keys(row[0].info).forEach(function(k) {
+		var cell = d3.select(g).selectAll(".cell_"+k)
+            .data(row.filter(function(d) { return d.info[k] || d.x==d.y; }))
+            .enter().append("rect")
+            .attr("class", function(d) {if(d.x==d.y) { return "cell cell_sep";} return "cell cell_"+k;} )
+            .attr("x", function(d, i) { return x(d.x); })
+            .attr("width", x.rangeBand())
+            .attr("height", x.rangeBand())
+            .style("fill-opacity", function(d) { if(d.x==d.y) { return 1;} return z[k](d.info[k]); })
+            .on("mouseover", mouseover)
+            .on("mouseout", mouseout)
+            .on("click", function (p) {
+                alert(symbols[p.y].name+'/'+symbols[p.x].name+'='+p.info[k]);
+            });
+	});
 }
 
-function mouseover(p) {
+function mouseover(p) 
+{
     d3.selectAll(".row text").classed("active", function(d, i) { return i == p.y; });
     d3.selectAll(".column text").classed("active", function(d, i) { return i == p.x; });
 }
 
-function mouseout() {
+function mouseout() 
+{
     d3.selectAll("text").classed("active", false);
 }
 
-function order(value) {
+function show(value)
+{
+	nodeKeys.forEach(function (k) {
+		if (k == value) {
+			var display = 'inline';
+		} else {
+			var display = 'none';
+		}
+		svg.selectAll('.cell_'+k)
+            .attr('display', display);
+	});
+}
+
+function order(value) 
+{
     x.domain(orders[value]);
     
-    var t = svg.transition().duration(2500);
-    
-    t.selectAll(".row")
-        .delay(function(d, i) { return x(i) * 4; })
+    svg.selectAll(".row")
         .attr("transform", function(d, i) { return "translate(0," + x(i) + ")"; })
       .selectAll(".cell")
-        .delay(function(d) { return x(d.x) * 4; })
         .attr("x", function(d) { return x(d.x); });
     
-    t.selectAll(".column")
-        .delay(function(d, i) { return x(i) * 4; })
+    svg.selectAll(".column")
         .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
 }
 
 d3.select("#order").on("change", function() {
     order(this.value);
 });
+d3.select('#show').on("change", function() {
+    show(this.value);
+});
+show('wt');
+order('wt');
 </script>
